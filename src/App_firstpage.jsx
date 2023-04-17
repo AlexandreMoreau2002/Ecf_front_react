025c05a3b6ef673e980e7dc50ff7fd65630b7203
AlexGrimpeur <moreaualexandre2002@gmail.com>
2023-04-17 21:10:11 +0200

    ++

---
+++ src/App_firstpage.jsx
@@ -0,0 +1,12 @@
+//premiere page, page par defaut 
+
+function App() {
+  return (
+   <div>
+      <h1>mon permier react</h1>
+      <p>accompagner de son petit paragraphe d'introduction</p>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <p>ceci est mon footer</p>
+      <p>cgu @moimeme</p>
+    </div>
+  )
+}
+
+export default Footer

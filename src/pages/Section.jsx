4b0d7ba9f00c47eb1c232b8c3a5c33f0fed6a5a9
AlexGrimpeur <moreaualexandre2002@gmail.com>
2023-05-30 21:17:29 +0200

    Amelioration du responsive des Articles

--- src/pages/Section.jsx
+++ src/pages/Section.jsx
@@ -1,29 +1,14 @@
 //premiere page, page par defaut 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Article from "../components/Article";
 
 function Section() {
   return (
     <>
       <Header/>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-      <section className="section">
-        <article className="article article1">
-          <i class="fa-solid fa-puzzle-piece"></i>
-          <h3>Lorem Ipsum</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, Fusce sed scelerisque turpis, nec feugiat leo. Duis efficitur eget sapien finibus sagittis</p>
-        </article>
-        <article className="article article2">
-          <i class="fa-solid fa-puzzle-piece"></i>
-          <h3>Lorem Ipsum</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, Fusce sed scelerisque turpis, nec feugiat leo. Duis efficitur eget sapien finibus sagittis</p>
-        </article>
-        <article className="article article3">
-          <i class="fa-solid fa-puzzle-piece"></i>
-          <h3>Lorem Ipsum</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, Fusce sed scelerisque turpis, nec feugiat leo. Duis efficitur eget sapien finibus sagittis</p>
-        </article>
-      </section>
+      <Article/>
       <h3>Contact us</h3>
       <form className="formSection" action="#" method="post">
         <div className="contactDetail">

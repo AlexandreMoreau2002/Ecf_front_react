54e7bdf87431761d774cade04e01d0018423cb2c
AlexGrimpeur <moreaualexandre2002@gmail.com>
2023-07-06 20:17:39 +0200

    Création du dossier Dev

---
+++ src/components/Article.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+
+
+function Article() {
+  return (
+    <section className="section">
+        <article className="article article1">
+            <i class="fa-solid fa-puzzle-piece"></i>
+            <h3>Lorem Ipsum</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, Fusce sed scelerisque turpis, nec feugiat leo. Duis efficitur eget sapien finibus sagittis</p>
+        </article>
+        <article className="article article2">
+            <i class="fa-solid fa-puzzle-piece"></i>
+            <h3>Lorem Ipsum</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, Fusce sed scelerisque turpis, nec feugiat leo. Duis efficitur eget sapien finibus sagittis</p>
+        </article>
+        <article className="article article3">
+            <i class="fa-solid fa-puzzle-piece"></i>
+            <h3>Lorem Ipsum</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, Fusce sed scelerisque turpis, nec feugiat leo. Duis efficitur eget sapien finibus sagittis</p>
+        </article>
+    </section>
+  )
+}
+
+export default Article
+
+
+
